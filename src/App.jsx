f97f9a919cb6743656014eeedb1cfc90e61da57b
nadasobeh22 <sobehnada2@gmail.com>
2025-02-25 00:35:03 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import Flights from './Pages/Flights.JSX';
 import Booking from './Pages/Booking';
